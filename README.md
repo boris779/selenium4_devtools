3d0234004a7eb9b01b63b61883160e9540d2b02f
exbwrubel <boris.wrubel@gmx.at>
2022-01-13 17:11:04 +0100

    Running version with hub and node via jar

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # selenium4_devtools
+
+started with the following commands:
+
+
+Hub:
+java -jar selenium-server-4.1.1.jar hub
+
+Node:
+java -jar selenium-server-4.1.1.jar node --config config.toml
--- src/main/java/at/co/boris/devtools/Main.java
+++ src/main/java/at/co/boris/devtools/Main.java
@@ -1,55 +1,51 @@
 package at.co.boris.devtools;
 
-import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.ImmutableCapabilities;
-import org.openqa.selenium.MutableCapabilities;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.HasDevTools;
-import org.openqa.selenium.devtools.v93.browser.Browser;
-import org.openqa.selenium.devtools.v93.browser.model.Bounds;
-import org.openqa.selenium.devtools.v93.browser.model.WindowID;
-import org.openqa.selenium.devtools.v93.browser.model.WindowState;
-import org.openqa.selenium.devtools.v93.log.Log;
-import org.openqa.selenium.remote.Augmenter;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
-import static org.openqa.selenium.remote.CapabilityType.BROWSER_VERSION;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v96.browser.Browser;
+import org.openqa.selenium.devtools.v96.browser.model.Bounds;
+import org.openqa.selenium.devtools.v96.browser.model.WindowID;
+import org.openqa.selenium.devtools.v96.browser.model.WindowState;
+import org.openqa.selenium.devtools.v96.log.Log;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedURLException {
+   public static void main(String[] args) throws MalformedURLException {
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability(BROWSER_VERSION, "93.0");
+      //DesiredCapabilities capabilities = new DesiredCapabilities();
+      //capabilities.setCapability("browserName", "chrome");
 
+      ChromeOptions options = new ChromeOptions();
+      options.setBrowserVersion("97");
 
+      // capabilities.setCapability(BROWSER_VERSION, "93.0");
+      // capabilities.setCapability(PLATFORM_NAME, Platform.LINUX);
 
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), capabilities);
+      WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
 
-        driver = new Augmenter().augment(driver);
+      driver = new Augmenter().augment(driver);
 
-        driver.get("https://www.duckduckgo.com");
-        DevTools devTools = ((HasDevTools) driver).getDevTools();
-        devTools.createSession();
+      driver.get("https://www.duckduckgo.com");
+      DevTools devTools = ((HasDevTools) driver).getDevTools();
+      devTools.createSession();
 
-        devTools.send(Log.enable());
-        devTools.send(Browser.setWindowBounds(new WindowID(1), new Bounds(
-                Optional.of(20),
-                Optional.of(20),
-                Optional.of(20),
-                Optional.of(20),
-                Optional.of(WindowState.NORMAL))));
+      devTools.send(Log.enable());
+      devTools.send(Browser.setWindowBounds(new WindowID(1),
+                                            new Bounds(Optional.of(20),
+                                                       Optional.of(20),
+                                                       Optional.of(20),
+                                                       Optional.of(20),
+                                                       Optional.of(WindowState.NORMAL))));
 
-        driver.quit();
+      driver.quit();
 
-
-    }
+   }
 }
