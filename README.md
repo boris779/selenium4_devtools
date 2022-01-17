677e5356cededcfd20d3dc082bbc2a87264c9442
exbwrubel <boris.wrubel@gmx.at>
2022-01-17 08:50:42 +0100

    Changes for the docker-compose version

--- README.md
+++ README.md
@@ -1,10 +1,5 @@
-# selenium4_devtools
+# selenium4_devtools Docker
 
 started with the following commands:
 
-
-Hub:
-java -jar selenium-server-4.1.1.jar hub
-
-Node:
-java -jar selenium-server-4.1.1.jar node --config config.toml
+docker-compose up
--- src/main/java/at/co/boris/devtools/Main.java
+++ src/main/java/at/co/boris/devtools/Main.java
@@ -4,6 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Optional;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
@@ -14,20 +15,17 @@ import org.openqa.selenium.devtools.v96.browser.model.WindowID;
 import org.openqa.selenium.devtools.v96.browser.model.WindowState;
 import org.openqa.selenium.devtools.v96.log.Log;
 import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class Main {
 
    public static void main(String[] args) throws MalformedURLException {
 
-      //DesiredCapabilities capabilities = new DesiredCapabilities();
-      //capabilities.setCapability("browserName", "chrome");
-
-      ChromeOptions options = new ChromeOptions();
-      options.setBrowserVersion("97");
-
-      // capabilities.setCapability(BROWSER_VERSION, "93.0");
-      // capabilities.setCapability(PLATFORM_NAME, Platform.LINUX);
+      //DesiredCapabilities caps = new DesiredCapabilities();
+      //caps.setPlatform(Platform.LINUX);
+      ChromeOptions options = new ChromeOptions(); //.merge(caps);
+      options.setBrowserVersion("96.0");
 
       WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
 
@@ -39,10 +37,10 @@ public class Main {
 
       devTools.send(Log.enable());
       devTools.send(Browser.setWindowBounds(new WindowID(1),
-                                            new Bounds(Optional.of(20),
-                                                       Optional.of(20),
-                                                       Optional.of(20),
-                                                       Optional.of(20),
+                                            new Bounds(Optional.of(80),
+                                                       Optional.of(80),
+                                                       Optional.of(80),
+                                                       Optional.of(80),
                                                        Optional.of(WindowState.NORMAL))));
 
       driver.quit();
