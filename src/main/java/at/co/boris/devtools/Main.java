18c8be6b79ba7d16022c2c4c092a8360ebc547a5
exbwrubel <boris.wrubel@gmx.at>
2022-01-25 16:03:41 +0100

    Update after reply from Juan Manuel Carames

--- src/main/java/at/co/boris/devtools/Main.java
+++ src/main/java/at/co/boris/devtools/Main.java
@@ -1,14 +1,14 @@
 package at.co.boris.devtools;
 
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Optional;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.*;
 import org.openqa.selenium.devtools.v96.browser.Browser;
 import org.openqa.selenium.devtools.v96.browser.model.Bounds;
 import org.openqa.selenium.devtools.v96.browser.model.WindowID;
@@ -25,9 +25,9 @@ public class Main {
       //DesiredCapabilities caps = new DesiredCapabilities();
       //caps.setPlatform(Platform.LINUX);
       ChromeOptions options = new ChromeOptions(); //.merge(caps);
-      options.setBrowserVersion("96.0");
+      //options.setBrowserVersion("96.0");
 
-      WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+      WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
 
       driver = new Augmenter().augment(driver);
 
@@ -46,4 +46,43 @@ public class Main {
       driver.quit();
 
    }
+
+   private void startDevToolsSerice(WebDriver driver) {
+      ChromeDevToolsService devtools;
+      // Init ChromeDevtools client
+      WebSocketService webSocketService;
+      try {
+         webSocketService = WebSocketServiceImpl.create(new URI(
+               String.format("ws://localhost:4444/devtools/%s/page", ((RemoteWebDriver) driver).getSessionId())));
+
+
+         CommandInvocationHandler commandInvocationHandler = new CommandInvocationHandler();
+         Map<Method, Object> commandsCache = new ConcurrentHashMap<>();
+
+
+         devtools = ProxyUtils.createProxyFromAbstract(ChromeDevToolsServiceImpl.class,
+                                                       new Class[] { WebSocketService.class, ChromeDevToolsServiceConfiguration.class },
+                                                       new Object[] { webSocketService, new ChromeDevToolsServiceConfiguration() },
+                                                       (unused, method, args) -> commandsCache.computeIfAbsent(method, key -> {
+                                                          Class<?> returnType = method.getReturnType();
+                                                          return ProxyUtils.createProxy(returnType, commandInvocationHandler);
+                                                       }));
+         commandInvocationHandler.setChromeDevToolsService(devtools);
+         System.out.println("started!");
+
+         //			testPrintNetworkRequests(devtools);
+         //testPrintPerformanceRequests(devtools);
+
+         devtools.close();
+
+         System.out.println("Finishes!");
+
+
+      } catch (Exception e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+
+
 }
